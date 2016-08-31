476aae0237c695ed5f5a89fdd86e08bbc3b73328
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-08-31 12:19:55 +0200

    FIX compilation

--- src/main/java/it/morfoza/EvenceController.java
+++ src/main/java/it/morfoza/EvenceController.java
@@ -1,3 +1,5 @@
+package it.morfoza;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -5,18 +7,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EvenceController {
- private EventsRepository eventsRepository;
+
+    private EventsRepository eventsRepository;
+
     @RequestMapping("/")
 
-    public String findEvent (Model model){
-        model.addAttribute("events", eventsRepository.getAllEvents() );
+    public String findEvent(Model model) {
+        model.addAttribute("events", eventsRepository.getAllEvents());
         return "formularz";
     }
+
     @RequestMapping("/wynikiWyszukiwania")
     public String dodajProdukt(@RequestParam(value = "dance", required = true) String dance,
                                @RequestParam(value = "city", required = true) String city,
-                               @RequestParam(value = "price", required = true) String price,Model model){
+                               @RequestParam(value = "price", required = true) String price, Model model) {
 
-    return "wynikiWyszukiwania";}
+        return "wynikiWyszukiwania";
+    }
 }
 
