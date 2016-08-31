b7c3f7210cf7b5f17072739b07ab62d0a562f291
widzew <klop99@wp.pl>
2016-08-31 14:21:43 +0200

    Event class

--- src/main/java/it/morfoza/Event.java
+++ src/main/java/it/morfoza/Event.java
@@ -1,6 +1,32 @@
 package it.morfoza;
 public class Event {
 
+    private String eventName;
+    private String city;
+    private String date;
+    private double price;
+    private String dance;
+
+    public Event (String eventName, String city, String date, double price, String dance) {
+        this.eventName = eventName;
+        this.city = city;
+        this.date = date;
+        this.price = price;
+        this.dance = dance;
+    }
+
+    public String toString() {
+        return eventName + city + date + " Cena " + price + " Taniec " + dance;
+    }
+
+    public boolean isFree() {
+        if (this.price == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
 
 }
