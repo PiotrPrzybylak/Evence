665830d3bb5c2e22a36ded50c49b2786c98c6f93
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-09-01 16:05:35 +0200

    Tests added

--- src/test/java/it/morfoza/test/HardcodedEventRepositoryTest.java
+++ src/test/java/it/morfoza/test/HardcodedEventRepositoryTest.java
@@ -8,22 +8,37 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class HardcodedEventRepositoryTest {
 
     @Test
-    public void shouldDisplayMatchingEvents(){
+    public void shouldDisplayMatchingEvents() {
 
-        //Given
-        HardcodedEventRepository sut= new HardcodedEventRepository();
+        // Given
+        HardcodedEventRepository sut = new HardcodedEventRepository();
 
-        //When
+        // When
         List<Event> events = sut.getByDance("salsa");
 
         // Then
         assertThat(events).hasSize(1);
         Event event1 = events.get(0);
         assertThat(event1.getEventName()).isEqualTo("Salsa Dance");
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenNoEventFound() {
+        // Given
+        HardcodedEventRepository sut = new HardcodedEventRepository();
+
+        try {
+            sut.getByDance("tralalala");
+            fail("should have exception");
+        } catch (RuntimeException e) {
+            // Then
+        }
     }
 
 }
