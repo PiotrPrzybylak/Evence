5643572690bc3105de6a2fc5e88d4e5fc16669f4
widzew <klop99@wp.pl>
2016-09-20 13:12:37 +0200

    Issue #3: In Money class double changed to BigDecimal

--- src/main/java/it/morfoza/Money.java
+++ src/main/java/it/morfoza/Money.java
@@ -1,18 +1,20 @@
 package it.morfoza;
 
+import java.math.BigDecimal;
+
 /**
  * Created by widzew on 2016-09-20.
  */
 public class Money {
 
-    private double value;
+    private BigDecimal value;
 
-    public Money(double value){
-        this.value = value;
+    public Money(double value) {
+        this.value = new BigDecimal(value);
     }
 
 
     public double getDoubleValue() {
-        return value;
+        return value.doubleValue();
     }
 }
